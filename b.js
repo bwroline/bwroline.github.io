cfce91610db39bc96b5169abb4c5d33329eb9483
DIC-DAKOYI <bwroline@gmail.com>
2023-07-20 14:02:41 +0100

    bwroline-update
    bwroline-update

---
+++ b.js
@@ -0,0 +1,116 @@
+// bwroline script
+var fp = 4.56655;
+f_p = fp.toExponential(2);
+var b_cnt = 0; // bwroline count
+var b_cnt_0 = 0;
+var num_classes = 0;
+var bwr_com = document.getElementById("bwroline.com").innerHTML;
+var shd_com = document.getElementById("shamudoents.com").innerHTML;
+var ghl_com = document.getElementById("great-hussein-legal-practitioners.com").innerHTML;
+var egb_com = document.getElementById("ecwa-goodnews-bwr.com").innerHTML;
+var btg_com = document.getElementById("btgees.com").innerHTML;
+var sd = new Date();
+var pros_gen = bwaya.length;
+var bwaya = new Array(bwr_com, shd_com, ghl_com, egb_com, btg_com);
+var sess = (new Date() - sd)
+var session = sess * (times * pros_gen.length));
+
+function times(a, b){
+    while username {
+        c = fp * f_p++;
+    }
+    return (c * (sess * session));
+}
+
+function PROS0line(name, phone, amount, dep_amount, depositor, date_time, website, salary, owner, bk, bk_numb){
+    this.own_name = name;
+    this.phn_num = phone;
+    this.pay_amount = amount;
+    this.curr_bal = dep_amount(new Date() + sess++);
+    this.per_deposit = depositor;
+    this.date_t = date_time;
+    this.web = website;
+    this.month_salary = salary;
+    this.comp_owner = owner;
+    this.bk_name = bk;
+    this.acc_numb = bk_numb;
+    this.PROS0lineDB;
+    this.sl;
+}
+
+function PROS0lineDB(){
+    ln1 = "<strong>Name: </strong>" + this.name + "<br/>";
+    ln2 = "<strong>Price amount: </strong> NGN" + this.amount + "<br/>";
+    ln3 = "<strong>Phone number: </strong>" + this.phone + "<br/>";
+    ln4 = "<strong>Amount deposited: </strong>" + this.dep_amount + "<br/>";
+    ln5 = "<strong>Depositor: </strong>" + this.depositor + "<br/>";
+    ln6 = "<strong>Date and time: </strong>" + this.date_time + "<br/>";
+    ln7 = "<strong>Website: </strong>" + this.website + "<br/>";
+    ln8 = "<strong>Salary: </strong>" + this.salary + "<br/>";
+    ln9 = "<strong>Owner: </strong>" + this.owner + "<br/>";
+    ln10 = "<strong>Bank: </strong>" + this.bk + "<br/>";
+    ln11 = "<strong>Number: </strong>" + this.bk_numb + "<br/>";
+    document.write(ln1, ln2, ln3, ln4, ln5, ln6, ln7, ln8, ln9, ln10, ln11);
+}
+
+// object instances for PROS0line
+po = new PROS0line("PROS0line", f_p, "", sess, sl, sess, bwaya[0, 1, 2, 3, 4], "", "DIB", ""); // come back to here
+bw = new PROS0line("bwroline", f_p, "08120744800", sess, sl, sess, bwaya[0, 1, 2, 3, 4], "", "DIC", "Unity", "0025531657");
+sh = new PROS0line("shamudoents", f_p, "07057606390", sess, sl, sess, bwaya[0, 1], "", "FSD", "");
+gh = new PROS0line("great hussein", f_p, "08057302254" ,sess, sl, sess, [0, 2], "", "NAJ", "Unity", "");
+eg = new PROS0line("ecwa goodnews", f_p, "08064648565", sess, sl, sess, [0, 3], "", "YIJ", "First", "");
+bt = new PROS0line("btgees", f_p, "09067600141", sess, sl, sess, [0, 4], "", "FC", "Unity", "");
+
+function sl(form, search){
+    this.search = search;
+    var usr = document.getElementById("sl").innerHTML =
+    var fn = form.elements["first name"].value + "." +
+    var ln = form.elements["last name"].value + "@bwroline.com" +
+    var ps = form.elements["password"].value + "[A-Zz-z0-9]+.";
+    var rs = form.reset();
+    form.elements["first"].focus();
+    var username = usr + require();
+
+    while (bwroline()){
+        document.write("\t \t <strong>PROS0line Globe Multi-links Media Unit</strong>");
+        if(username.search == "bwroline"){
+            var url = document.URL;
+            document.write("\n <br><strong>URL: </strong>" + url + "\n <br><strong>Name: </strong>" + this.name + "\n <br><strong>Website: </strong>" + bwaya[0]);
+        }
+        else if(username.search == "shamudoents"){
+            var url = document.URL;
+            document.write("\n <br><strong>URL: </strong>" + url + "\n <br><strong>Name: </strong>" + this.name + "\n <br><strong>Website: </strong>" + bwaya[1]);
+        }
+        else if(username.search == "great-Hussein-Legal-Practitioners"){
+            var url = document.URL;
+            document.write("\n <br><strong>URL: </strong>" + url + "\n <br><strong>Name: </strong>" + this.name + "\n <br><strong>Website: </strong>" + bwaya[2]);
+        }
+        else if(username.search == "ecwa-goodnews-bwari"){
+            var url = document.URL;
+            document.write("\n <br><strong>URL: </strong>" + url + "\n <br><strong>Name: </strong>" + this.name + "\n <br><strong>Website: </strong>" + bwaya[3]);
+        }
+        else if(username.search == "btgees"){
+            var url = document.URL;
+            document.write("\n <br><strong>URL: </strong>" + url + "\n <br><strong>Name: </strong>" + this.name + "\n <br><strong>Website: </strong>" + bwaya[4]);
+        }
+        else{
+            document.write("input text to search");
+        }
+    }
+}
+
+function Update(){
+    var upto = Array("");
+    while upto{
+        return (PROS0line + upto + upto);
+    }
+}
+
+function bwroline(){
+    if(bwroline){
+        while (bwroline == bwaya[0]) {
+            document.write(po++)
+        }
+        return (po);
+    }
+}
